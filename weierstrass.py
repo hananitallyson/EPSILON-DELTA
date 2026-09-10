f507b05254dad19b4edcb6974244e628bfe96a02
hananitallyson <tallysonhanani@gmail.com>
2026-09-10 10:02:39 -0300

    weierstrass added

---
+++ weierstrass.py
@@ -0,0 +1,48 @@
+import math
+
+
+def weierstrass(f, a, L, epsilon=0.001):
+    delta = 1.0
+
+    for _ in range(100):
+        valid = True
+
+        for i in range(1, 1001):
+            x = a + delta * i / 1000
+
+            if math.fabs(f(x) - L) >= epsilon:
+                valid = False
+                break
+
+        if valid:
+            return True, delta
+
+        delta /= 2
+
+    return False, None
+
+
+def cauchy(f, a, L, epsilon=0.5):
+    delta = 1.0
+
+    for _ in range(100):
+        for i in range(1, 1001):
+            x = a + delta * i / 1000
+
+            if math.fabs(f(x) - L) >= epsilon:
+                return x
+
+        delta /= 2
+
+    return None
+
+
+f = lambda x: 2 * x
+g = lambda x: 1 if x >= 0 else -1
+
+data = input("inputs (a L): ").split(" ")
+target = float(data[0]) 
+lim = float(data[1])
+
+print("limit exists:", weierstrass(f, target, lim), "| counterexample:", cauchy(g, target, lim))
+
